6fd4d96e2b0565e859e8b05237b05a8eb459f21a
cx85 <evelynnscripts@gmail.com>
2026-02-14 15:22:41 +0200

    Update mvsd-wk.lua

--- mvsd-wk.lua
+++ mvsd-wk.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/evelynnscripts/vexal/refs/heads/main/mvsd.lua"))()
+setclipboard("https://discord.gg/nMFz2Sq45J")
+task.wait(1)
+game:GetService("Players").LocalPlayer:Kick("this script is outdated.\njoin https://discord.gg/nMFz2Sq45J for latest (link copied)")
